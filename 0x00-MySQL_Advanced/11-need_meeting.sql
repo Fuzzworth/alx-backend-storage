60dca5a494c1ecfc53ce7ca8e2be9032b00f62eb
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-18 23:30:25 +0200

    Task 11 FIX: 1

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,7 +2,7 @@
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE 
-score < 80 OR 
-last_meeting IS NULL OR 
+WHERE
+score < 80 OR
+last_meeting IS NULL OR
 last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH);
