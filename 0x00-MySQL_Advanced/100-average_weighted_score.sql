6cd49bc67594c132ae1cd0dd4fe1a1d99bbf4e88
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-18 23:37:49 +0200

    Task 12 FIX: 1

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,18 +7,19 @@ BEGIN
 
     -- Calculate the weighted average
     SELECT SUM(score * weight) INTO total_score, SUM(weight) INTO total_weight
-    FROM corrections
-    WHERE user_id = user_id;
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
 
-    -- Update the average_weighted_score in the users table
+    -- Update the average_score in the users table
     IF total_weight > 0 THEN
         UPDATE users
-        SET average_weighted_score = total_score / total_weight
+        SET average_score = total_score / total_weight
         WHERE id = user_id;
     ELSE
         -- Handle the case when total_weight is 0 to avoid division by zero
         UPDATE users
-        SET average_weighted_score = 0
+        SET average_score = 0
         WHERE id = user_id;
     END IF;
 END //
