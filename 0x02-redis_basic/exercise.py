e1a63c5f74dd5e9b560f4ae0486cef4776209e5b
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-24 23:00:38 +0200

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,12 +27,12 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn = None) -> Any:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
         '''
         Function Docs
         '''
         value = self._redis.get(key)
-        if value and type(fn) is not None:
+        if value and fn is not None:
             return fn(value)
 
         return value
