602509cca4339c5fb966f5e84198d14025afb320
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-24 22:55:48 +0200

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Modules Docs
 '''
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable,Any
 
 
 class Cache:
@@ -26,3 +26,13 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        '''
+        Function Docs
+        '''
+        value = self._redis.get(key)
+        if value and type(fn) is not None:
+            return fn(value)
+
+        return value
