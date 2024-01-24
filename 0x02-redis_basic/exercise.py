d1c42186f82286ed150ea9dfeb2a20f50657ad4f
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-24 22:38:42 +0200

    Task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 Modules Docs
 '''
 import redis
+from uuid import uuid4
 
 
 class Cache:
@@ -16,3 +17,11 @@ class Cache:
         '''
         self._redis = redis.Redis()
         self._redis.flushdb()
+
+    def store(self, data) -> str:
+        '''
+        Function Docs
+        '''
+        random_key = uuid4()
+        self._redis.set(random_key, data)
+        return random_key
